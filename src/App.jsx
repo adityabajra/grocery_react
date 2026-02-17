00b98ca78ae1474bba43d9d0d38d602b55bf6264
adityabajra <adityabajracharya18@gmail.com>
2026-02-17 23:00:52 +0545

    added delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Items from "./components/Items";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { groceryItems } from "./data/groceryItems.js";
 
 const App = () => {
@@ -17,11 +19,21 @@ const App = () => {
     });
     setItems(newItems);
   };
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    toast.success("item deleted");
+  };
 
   return (
     <>
       <section className="section-center">
-        <Items items={groceryItems} />
+        <ToastContainer position="top-center" />
+        <Items
+          items={items}
+          editCompleted={editCompleted}
+          removeItem={removeItem}
+        />
       </section>
     </>
   );
